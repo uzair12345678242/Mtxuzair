56b94eed6787d801612241782344876a5f6e7b95
chandu7295 <ck445677@gmail.com>
2024-08-16 00:24:50 +0530

    Update SHANKAR.js

--- SHANKAR.js
+++ SHANKAR.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 //////////////////////////////////////////////////
 //    Yêu cầu tất cả các nhu cầu sử dụng biến   //
 //////////////////////////////////////////////////
