2bb8e2ff36608f25afb69b87d2d95092fe5a470c
sk7295 <167713397+sk7295@users.noreply.github.com>
2024-08-19 01:19:40 +0530

    Update antiout.js

--- modules/events/antiout.js
+++ modules/events/antiout.js
@@ -1,31 +1,22 @@
 module.exports.config = {
-    name: "antiout",
-    eventType: ["log:unsubscribe"],
-    version: "1.0.7",
-    credits: "ProCoderMew",
-    description: "Listen events",
-    dependencies: {
-        "path": ""
-    }
+ name: "antiout",
+ eventType: ["log:unsubscribe"],
+ version: "0.0.1",
+ credits: "SHANKAR SUMAN",
+ description: "Listen events"
 };
 
-module.exports.run = async function ({ api, event, Users }) {
-    const { resolve } = global.nodemodule["path"];
-    const path = resolve(__dirname, '../commands', 'hethong', 'antiout.json');
-    const { antiout } = require(path);
-    const { logMessageData, author, threadID } = event;
-    const id = logMessageData.leftParticipantFbId;
-    if (author == id && id != api.getCurrentUserID()) {
-        const name = await Users.getNameUser(id) || "Người dùng Facebook";
-        if (antiout.hasOwnProperty(threadID) && antiout[threadID] == true) {
-            try {
-                await api.addUserToGroup(id, threadID);
-                return api.sendMessage(`[ 𝗔𝗡𝗧𝗜𝗢𝗨𝗧 ] 𝗞𝗶́𝗰𝗵 𝗛𝗼𝗮̣𝘁 𝗔𝗻𝘁𝗶𝗼𝘂𝘁 𝗧𝗵𝗮̀𝗻𝗵 𝗖𝗼̂𝗻𝗴 𝗕𝗼𝘁 Đ𝗮̃ 𝗧𝗵𝗲̂𝗺 𝗡𝗴𝘂̛𝗼̛̀𝗶 𝗗𝘂̀𝗻𝗴 => ${name}`, threadID);
-            }
-            catch (e) {
-                return api.sendMessage(`[ 𝗔𝗡𝗧𝗜𝗢𝗨𝗧 ] 𝗞𝗵𝗼̂𝗻𝗴 𝘁𝗵𝗲̂̉ 𝘁𝗵𝗲̂𝗺 ${name} 𝘃𝘂̛̀𝗮 𝗼𝘂𝘁 𝘃𝗮̀𝗼 𝗹𝗮̣𝗶 𝗻𝗵𝗼́𝗺.`, threadID);
-            }
-        }
-    }
-    return;
-      }
+module.exports.run = async({ event, api, Threads, Users }) => {
+ let data = (await Threads.getData(event.threadID)).data || {};
+ if (data.antiout == false) return;
+ if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+ const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
+ if (type == "self-separation") {
+  api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
+   if (error) {
+    api.sendMessage(`Unable to re-add member ${name} to the group!`, event.threadID)
+   } else api.sendMessage(`Active antiout mode, ${name} has been re-added to the group!`, event.threadID);
+  })
+ }
+}
