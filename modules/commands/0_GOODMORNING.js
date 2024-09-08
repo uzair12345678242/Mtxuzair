8faadb7c899c849ece0e93df817ac752dd16261c
sk7295 <167713397+sk7295@users.noreply.github.com>
2024-09-08 06:33:43 +0530

    Update 0_GOODMORNING.js

--- modules/commands/0_GOODMORNING.js
+++ modules/commands/0_GOODMORNING.js
@@ -1,29 +1,63 @@
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+
 module.exports.config = {
-  name: "gm",
-    version: "2.1.1",
-  hasPermssion: 0,
-  credits: "SHANKAR SUMAN", 
-  description: "Just Respond",
-  commandCategory: "no prefix",
-    cooldowns: 5, 
+		name: "good-morning",
+		version: "1.0.1",
+		hasPermssion: 0,
+		credits: "SHANKAR SUMAN",
+		description: "no prefix",
+	usePrefix: false,
+		commandCategory: "No command marks needed",
+		usages: "Yo Yo",
+		cooldowns: 5,
 };
 
-module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
-  var name = await Users.getNameUser(event.senderID);
-  var { threadID, messageID } = event;
-  let react = event.body.toLowerCase();
-  if(react.includes("good morning") ||
-     react.includes("gud morning") || react.includes("gm") || react.includes("GM") ||
-react.includes("Gm") ||
-react.includes("GOOD MORNING")) {
-    var msg = {
-        body: `${name} 𝐕𝐄𝐑𝐘 𝐆𝐎𝐎𝐃 𝐌𝐎𝐑𝐍𝐈𝐍𝐆😇 बाबू`,attachment: fs.createReadStream(__dirname + `/shankar/gm.gif`)
-      }
-      api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("☀️", event.messageID, (err) => {}, true)
-    }
-  }
-  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+const gifs = [
+		"https://i.imgur.com/i8F70T9.gif",
+		"https://i.imgur.com/yhmCAjC.gif",
+		"https://i.imgur.com/hOc04wc.gif",
+		"https://i.imgur.com/aQgpocY.gif",
+		"https://i.imgur.com/pzLj6IX.gif"
+];
 
-  }
+const messages = [
+		"🌄ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ, {name}! ᴛʀʏ ꜱᴏᴍᴇ ᴄᴏꜰꜰᴇᴇ ᴏʀ ᴛᴇᴀ ᴛᴏ ᴡᴀᴋᴇ ʏᴏᴜ ᴜᴘ☀️☕",
+		"🌅ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ, {name}! ʜᴀᴠᴇ ᴀ ᴡᴏɴᴅᴇʀꜰᴜʟ ᴅᴀʏ✨",
+		"🌞ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ, {name}! ʟᴇᴛ'ꜱ ᴍᴀᴋᴇ ᴛʜɪꜱ ᴅᴀʏ ᴀᴡᴇꜱᴏᴍᴇ🌻",
+		"VERY GOOD MORNING {name} BABU🌄",
+		"very good morning {name} babu🌄",
+		"good morning {name} babu🌄"
+];
+
+module.exports.handleEvent = async function({ api, event, client, Users, __GLOBAL }) {
+		var { threadID, messageID } = event;
+		var name = await Users.getNameUser(event.senderID);
+
+		if (event.body.toLowerCase().startsWith("good morning") || 
+				event.body.toLowerCase().startsWith("gm") || 
+				event.body.toLowerCase().startsWith("morning") || 
+				event.body.toLowerCase().startsWith("GM") || 
+				event.body.toLowerCase().startsWith("GOOD MORNING")) { 
+
+				// Select random GIF and message
+				const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
+				const randomMessage = messages[Math.floor(Math.random() * messages.length)].replace("{name}", name);
+				const downloadPath = path.join(__dirname, 'Good-Morning-Gif-Images.gif');
+
+				// Download image from Imgur
+				request(randomGif).pipe(fs.createWriteStream(downloadPath)).on('close', () => {
+						var msg = {
+								body: randomMessage,
+								attachment: fs.createReadStream(downloadPath)
+						};
+						api.sendMessage(msg, threadID, messageID);
+						api.setMessageReaction("🌇", event.messageID, (err) => {}, true);
+				});
+		}
+}
+
+module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
