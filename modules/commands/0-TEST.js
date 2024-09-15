760fa8fae31d7bfea26f0cb5356c293175d14ab7
Shankar Suman <shankarproject29@gmail.com>
2024-09-15 22:56:12 +0530

    Update 0-TEST.js

--- modules/commands/0-TEST.js
+++ modules/commands/0-TEST.js
@@ -1,53 +1,94 @@
-const axios = require('axios');
-const fs = require('fs-extra');
-const path = require('path');
-
-module.exports = {
+module.exports.config = {
   name: "test",
-  Programmer: "August Quinn",
-  info: "Generate images with DALL-E.",
-  hasPermission: "members",
-  category: "AI",
-  usage: "/dalle [text]",
-  cooldowns: 5,
-  prefix: "disable",
-  letStart: async function ({ pushMessage, target, event}) {
-    try {
-      const text = target.join(" ");
-
-      const apiUrl = 'http://openai-dall-e.august-quinn-api.repl.co/generate-images';
-      const response = await axios.post(apiUrl, { text, num_images: 4 });
-
-      const imgData = [];
-
-      for (let i = 0; i < response.data.openai.items.length; i++) {
-        const imgUrl = response.data.openai.items[i].image_resource_url;
-
-        if (imgUrl) {
-          const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-          const imgPath = path.join(__dirname, 'cache', `dalle_${i + 1}.jpg`);
-
-          await fs.outputFile(imgPath, imgResponse.data);
-          imgData.push(fs.createReadStream(imgPath));
-
-          if (fs.existsSync(imgPath)) {
-            await fs.unlink(imgPath);
-          }
-        }
-      }
-
-      if (imgData.length > 0) {
-        await pushMessage.reply({
-          body: `Generated Images with DALL-E:`,
-          attachment: imgData
-        }, event.threadID);
-      } else {
-        await pushMessage.reply('No images generated.', event.threadID);
-      }
-
-    } catch (error) {
-      console.error(error);
-      await pushMessage.reply(`Image generation failed!\nError: ${error.message}`, event.threadID);
-    }
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "SHANKAR SUMAN",
+  description: "Play music from youtube",
+  commandCategory: "utility",
+  usages: "[title]",
+  cooldowns: 10,
+  dependencies: {
+            
   }
 };
+
+module.exports.run = async({api, event}) => {
+	const axios = require("axios");
+    const fs = require("fs-extra");
+    const Innertube = require("youtubei.js");
+    const request = require("request");
+    let input = event.body;
+    
+    var text = input;     text = text.substring(7)
+let data = input.split(" ");
+  
+if (data.length < 2) {               return api.sendMessage("⚠️Please put a title or name of the music.", event.threadID);
+}
+  
+
+data.shift()
+
+
+  const youtube = await new Innertube();
+ 
+  const search = await youtube.search(text);
+if (search.videos[0] === undefined){
+api.sendMessage("Error: Invalid request.",event.threadID,event.messageID)
+api.setMessageReaction("❎", event.messageID, (err) => {}, true)
+}else{
+api.sendMessage(`🔎Searching for "${text}"...`,  event.threadID,event.messageID);
+api.setMessageReaction("✅", event.messageID, (err) => {}, true)
+var timeleft = 3;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+
+    }
+  timeleft -= 1;
+}, 1000);
+  const stream = youtube.download(search.videos[0].id, {
+    format: 'mp4',
+    type: 'audio',
+    audioQuality: 'lowest',
+    loudnessDB: '20',
+    audioBitrate: '320',
+    fps: '30'
+  });
+  
+stream.pipe(fs.createWriteStream(__dirname + `/cache/${search.videos[0].title}.mp3`))
+
+
+  stream.on('start', () => {
+    console.info('[DOWNLOADER]', 'Starting download now!');
+  }); 
+  stream.on('info', (info) => {
+    console.info('[DOWNLOADER]',`Downloading ${info.video_details.title} by ${info.video_details.metadata.channel_name}`);
+    console.log(info)
+  });
+
+  
+  stream.on('end', () => {
+  // process.stdout.clearLine();
+  // process.stdout.cursorTo(0);
+    console.info(`[DOWNLOADER] Downloaded`)
+    
+    
+    var message = {
+          body:("Here's your music, enjoy!🥰\n\nTitle: "+search.videos[0].title),
+         attachment:[ 
+fs.createReadStream(__dirname + `/cache/${search.videos[0].title}.mp3`)]}
+           api.sendMessage(message, event.threadID,event.messageID);
+  }); 
+stream.on('error', (err)=> console.error('[ERROR]',err))
+
+         stream.on('end', async () => {  
+           
+           if (fs.existsSync(__dirname + `/cache/${search.videos[0].title}.mp3`)) {
+                                    fs.unlink(__dirname + `/cache/${search.videos[0].title}.mp3`, function (err) {
+                                  if (err) console.log(err);                                        
+                                  console.log(__dirname + `/cache/${search.videos[0].title}.mp3 is deleted!`);
+                                                        });
+                                                     }
+             })
+}
+      } 
