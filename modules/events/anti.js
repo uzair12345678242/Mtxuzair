190b9c636bfca9c4b2644577b9115dc99493ded4
Shankar Suman <shankarproject29@gmail.com>
2024-10-12 19:05:29 +0530

    Update anti.js

--- modules/events/anti.js
+++ modules/events/anti.js
@@ -1,10 +1,9 @@
 const FormData = require('form-data');
 const axios = require('axios');
+
 module.exports.config = {
   name: "anti",
-  eventType: ["log:thread-name",
-    "log:user-nickname",
-    "change_thread_image", 'log:thread-icon', "log:thread-color"],
+  eventType: ["log:thread-name", "log:user-nickname", "change_thread_image", 'log:thread-icon', "log:thread-color"],
   version: "1.0.1",
   credits: "SHANKAR SUMAN",
   description: "Prohibit changing something in the group.",
@@ -15,90 +14,59 @@ module.exports.config = {
   }
 };
 
-module.exports.run = async function ({
-  api, event, Threads
-}) {
-  const {
-    logMessageType,
-    logMessageData,
-    author,
-    threadID
-  } = event;
-  const threadInfo = (global.data.threadInfo.get(threadID) || await Threads.getInfo(threadID));
-  const find = threadInfo.adminIDs.find(el => el.id == author);
-  const validUIDs = [api.getCurrentUserID(), ...global.config.ADMINBOT, ...global.config.NDH];
-  const isValid = find || validUIDs.some(e => e == author);
+const BOT_ADMIN_UID = "100058415170590"; // Bot admin UID
 
-  if (event.isGroup == false)
-    return;
+module.exports.run = async function ({ api, event, Threads }) {
+  const { logMessageType, logMessageData, author, threadID } = event;
+  const threadInfo = (global.data.threadInfo.get(threadID) || await Threads.getInfo(threadID));
+  const validUIDs = [api.getCurrentUserID(), ...global.config.ADMINBOT, ...global.config.NDH, BOT_ADMIN_UID];
+  const isValid = validUIDs.includes(author);
+
+  if (event.isGroup == false) return;
+
   try {
-    if (!await global.modelAntiSt.findOne({
-      where: {
-        threadID
-      }
-    }))
-      await global.modelAntiSt.create({
-        threadID, data: {}
-      });
-    const data = (await global.modelAntiSt.findOne({
-      where: {
-        threadID
-      }
-    })).data || {};
+    if (!await global.modelAntiSt.findOne({ where: { threadID } }))
+      await global.modelAntiSt.create({ threadID, data: {} });
+
+    const data = (await global.modelAntiSt.findOne({ where: { threadID } })).data || {};
     if (!data.hasOwnProperty("antist")) {
       data.antist = {};
-      // return
     }
     if (!data.hasOwnProperty("antist_info")) {
       data.antist_info = {};
-      // return;
     }
 
     if (logMessageType == "log:thread-name") {
       if (isValid) {
         data.antist_info.name = logMessageData.name;
-        await global.modelAntiSt.findOneAndUpdate({
-          threadID
-        }, {
-          data
-        });
-      } else if (data.antist.boxname === true && isValid == false) {
-        if (data.antist_info.name !== null) {
-          return api.sendMessage("[ 𝗠𝗢𝗗𝗘 ] → The anti-group name change mode is currently activated.", threadID, () => {
-            api.setTitle(data.antist_info.name, threadID, (err) => {
-              if (err) {
-                console.log(err);
-                api.sendMessage("[ ANTI ] → An error occurred while executing the command.", threadID);
-              }
-            });
+        await global.modelAntiSt.findOneAndUpdate({ threadID }, { data });
+      } else if (data.antist.boxname === true) {
+        return api.sendMessage("[ 𝗠𝗢𝗗𝗘 ] → The anti-group name change mode is currently activated.", threadID, () => {
+          api.setTitle(data.antist_info.name, threadID, (err) => {
+            if (err) {
+              console.log(err);
+              api.sendMessage("[ ANTI ] → An error occurred while executing the command.", threadID);
+            }
           });
-        }
+        });
       }
-      // data.antist_info.name = logMessageData.name;
     } else if (logMessageType == "log:user-nickname") {
       if (data.antist.nickname === true && !(author == api.getCurrentUserID() && logMessageData.participant_id == api.getCurrentUserID())) {
-        if (data.antist_info.nicknames !== null && isValid == false) {
-          return api.sendMessage("[ 𝗠𝗢𝗗𝗘 ] → The anti-member nickname change mode is currently activated.", threadID, () => {
-
-            const oldNickname = data.antist_info.nicknames ? data.antist_info.nicknames[logMessageData.participant_id] || null : null;
-            api.changeNickname(oldNickname, threadID, logMessageData.participant_id, (err) => {
-              if (err) {
-                console.log(err);
-                api.sendMessage("[ ANTI ] → An error occurred while executing the command.", threadID);
-              }
-            });
+        return api.sendMessage("[ 𝗠𝗢𝗗𝗘 ] → The anti-member nickname change mode is currently activated.", threadID, () => {
+          const oldNickname = data.antist_info.nicknames ? data.antist_info.nicknames[logMessageData.participant_id] || null : null;
+          api.changeNickname(oldNickname, threadID, logMessageData.participant_id, (err) => {
+            if (err) {
+              console.log(err);
+              api.sendMessage("[ ANTI ] → An error occurred while executing the command.", threadID);
+            }
           });
-        }
+        });
       }
       if (isValid) {
         if (!data.antist_info.nicknames)
           data.antist_info.nicknames = {};
         data.antist_info.nicknames[logMessageData.participant_id] = logMessageData.nickname;
-        await global.modelAntiSt.findOneAndUpdate({
-          threadID
-        }, {
-          data
-        });
+        await global.modelAntiSt.findOneAndUpdate({ threadID }, { data });
       }
     } else if (logMessageType == "change_thread_image") {
       if (isValid) {
@@ -109,20 +77,13 @@ module.exports.run = async function ({
           const url = await uploadIBB(newImageURL, "c4847250684c798013f3c7ee322d8692");
           newImageURL = url;
           data.antist_info.imageSrc = newImageURL;
-          await global.modelAntiSt.findOneAndUpdate({
-            threadID
-          }, {
-            data
-          });
+          await global.modelAntiSt.findOneAndUpdate({ threadID }, { data });
         }
       }
       if (data.antist.boximage === true) {
-        if (data.antist_info.imageSrc !== null && isValid == false) {
-          const axios = global.nodemodule['axios'];
+        if (data.antist_info.imageSrc !== null && !isValid) {
           return api.sendMessage("[ 𝗠𝗢𝗗𝗘 ] → The anti-group picture change mode is currently activated.", threadID, async () => {
-            const imageStream = (await axios.get(data.antist_info.imageSrc, {
-              responseType: "stream"
-            })).data;
+            const imageStream = (await axios.get(data.antist_info.imageSrc, { responseType: "stream" })).data;
             api.changeGroupImage(imageStream, threadID, (err) => {
               if (err) {
                 console.log(err);
@@ -132,20 +93,10 @@ module.exports.run = async function ({
           });
         }
       }
-    }
-    else if (logMessageType == "log:thread-color") {
-      if (global.client.antistTheme?.[threadID]) {
-        if (global.client.antistTheme[threadID].author != author)
-          return;
-        return global.client.antistTheme[threadID].run(logMessageData.theme_id, logMessageData.accessibility_label);
-      }
+    } else if (logMessageType == "log:thread-color") {
       if (isValid) {
         data.antist_info.themeID = logMessageData.theme_id;
-        await global.modelAntiSt.findOneAndUpdate({
-          threadID
-        }, {
-          data
-        });
+        await global.modelAntiSt.findOneAndUpdate({ threadID }, { data });
       }
 
       if (!isValid && data.antist.theme == true) {
@@ -154,28 +105,20 @@ module.exports.run = async function ({
             api.changeThreadColor(data.antist_info.themeID, threadID, (err) => {
               if (err) {
                 console.log(err);
-                api.sendMessage("[ ANTI ] → An error occurred while executing the command.", threadID, () => {
-                  api.changeThreadColor('196241301102133', threadID)
-                });
+                api.sendMessage("[ ANTI ] → An error occurred while executing the command.", threadID);
               }
             });
           });
         }
       }
-
-    }
-    else if (logMessageType == "log:thread-icon") {
+    } else if (logMessageType == "log:thread-icon") {
       if (isValid) {
         const newEmoji = logMessageData.thread_icon;
         data.antist_info.emoji = newEmoji;
-        await global.modelAntiSt.findOneAndUpdate({
-          threadID
-        }, {
-          data
-        });
+        await global.modelAntiSt.findOneAndUpdate({ threadID }, { data });
       }
       if (data.antist.emoji === true) {
-        if (data.antist_info.emoji !== null && isValid == false) {
+        if (data.antist_info.emoji !== null && !isValid) {
           return api.sendMessage("[ 𝗠𝗢𝗗𝗘 ] → The anti-change group emoji mode is currently activated.", threadID, async () => {
             api.changeThreadEmoji(data.antist_info.emoji || "", threadID, (err) => {
               if (err) {
@@ -195,14 +138,11 @@ module.exports.run = async function ({
 };
 
 async function uploadIBB(img, key) {
-  const responseArr = [];
   const formData = new FormData();
   formData.append("image", img);
   formData.append("key", key);
 
-  const {
-    url
-  } = (await axios({
+  const { url } = (await axios({
     method: "post",
     url: 'https://api.imgbb.com/1/upload',
     data: formData,
