23b435b3ccd7c1adab079f5c304f884495c9e67b
sk7295 <167713397+sk7295@users.noreply.github.com>
2024-10-17 13:37:25 +0530

    Update npm.js

--- modules/commands/npm.js
+++ modules/commands/npm.js
@@ -1,41 +1,146 @@
-module.exports.config = {
-  name: "npm",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "SHANKAR",
-  description: "",
-  commandCategory: "user",
-  usages: "npm",
-  cooldowns: 5
-};
+const https = require('https');
+class NpmCommand {
+    static config = {
+        name: "npm",
+        version: "1.0.0",
+        hasPermssion: 3,
+        credits: "SHANKAR",
+        description: "View package information on npm",
+        commandCategory: "Admin",
+        usages: "[package]",
+        cooldowns: 5,
+        dependencies: {
+            "https": ""
+        }
+    };
 
-module.exports.run = async function ({ api, event, args }) {
-  const axios = require('axios');
-  var cc = args.join(" ");
+    static async run({ api, event, args }) {
+        const [packageName, page = 1] = args;
+        const limit = 5;
+        const offset = (Number(page) - 1) * limit;
 
-  // Checking if the user has provided a package name
-  if (!cc) return api.sendMessage(`Please enter the name of the package you want to search for!`, event.threadID, event.messageID);
+        if (!packageName) {
+            return api.sendMessage("Please enter the name of the package you want to search for 🌸", event.threadID, event.messageID);
+        }
 
-  // Making the API request using axios
-  try {
-    const res = await axios.get(`https://npm-packages-list-api.p.rapidapi.com/suggestions?q=${encodeURIComponent(cc)}`, {
-      headers: {
-        'x-rapidapi-host': 'npm-packages-list-api.p.rapidapi.com',
-        'x-rapidapi-key': '6734af4409msh8d5419367b0d679p14dcb3jsn113f61b581db' // Your API Key
-      }
-    });
+        try {
+            const response = await NpmCommand.getPackage(packageName);
+            const packages = response.data.objects.slice(offset, offset + limit);
 
-    // Extracting the response data
-    const a = res.data[0].name;  // Name of the first suggested package
-    const b = res.data[0].description;  // Description of the package
-    const d = res.data[0].keywords;  // Keywords associated with the package
+            if (!packages.length) {
+                return api.sendMessage("No results found matching your keyword 😭", event.threadID, event.messageID);
+            }
+            else if (packages.length == 1) {
+                const { name, version, keywords, description, links, author: {name: authorName}, date, publisher } = packages[0].package;
+                const { homepage } = links;
+                const { username, email } = publisher;
+                return api.sendMessage(`Name: ${name}\nVersion: ${version}\nDescription: ${description}\nCreated on: ${date}\nAuthor: ${authorName}\nHomepage: ${homepage}\nKeywords: ${keywords}\nMaintained by: ${username}\nEmail: ${email}\nReact "❤️" if you want to install the package\nReact "👎" if you want to cancel`, event.threadID, ((error, info) => {
+                    global.client.handleReaction.push({
+                        name: NpmCommand.config.name,
+                        messageID: info.messageID,
+                        author: event.senderID,
+                        type: "confirm",
+                        package: packages[0]
+                    });
+                }), event.messageID);
+            }
 
-    // Sending the response back to the user
-    if (!a || !b) return api.sendMessage(`Package does not exist or no data available`, event.threadID, event.messageID);
-    return api.sendMessage({ body: `[💙]━━『 PACKAGE INFO 』━━[💙]\n\n==== Package: ${cc} ====\n→ Name: ${a}\n→ Description: ${b}\n→ Keywords: ${d ? d.join(', ') : 'No keywords'}` }, event.threadID, event.messageID);
+            const msg = Array.from({ length: packages.length }, (_, index) => {
+                const { name, version, keywords, description, links, author: {name: authorName}, date, publisher } = packages[index].package;
+                const { homepage } = links;
+                const { username, email } = publisher;
+                return `${index + 1}. Name: ${name}\nVersion: ${version}\nDescription: ${description}\nCreated on: ${date}\nAuthor: ${authorName}\nHomepage: ${homepage}\nKeywords: ${keywords}\nMaintained by: ${username}\nEmail: ${email}\n━━━━━━━━━━━━━\n`;
+            });
 
-  } catch (error) {
-    console.error(error);
-    return api.sendMessage(`There was an error retrieving the package data.`, event.threadID, event.messageID);
-  }
-};
+            api.sendMessage(`${msg.join('')}👉 Reply to this message with the corresponding number of the package you want to install\n-------- Page ${page}/${Math.ceil(response.data.objects.length / limit)} --------`, event.threadID, async (error, info) => {
+                global.client.handleReply.push({
+                    name: NpmCommand.config.name,
+                    messageID: info.messageID,
+                    author: event.senderID,
+                    type: "package",
+                    package: packages
+                });
+            }, event.messageID);
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+            api.sendMessage(`An error occurred while fetching information for package ${packageName} 😿`, event.threadID);
+        }
+    }
+
+    static async handleReply({ api, event, handleReply }) {
+        const { author, type, package: packages } = handleReply;
+        if (event.senderID !== author || type !== "package") return;
+        const packageIndex = parseInt(event.body, 10);
+        if (!isNaN(packageIndex) && packageIndex >= 1 && packageIndex <= packages.length) {
+            const packageName = packages[packageIndex - 1]?.package?.name;
+            api.sendMessage(`Are you sure you want to install the package ${packageName}?\nReact "❤️" if you want to install`, event.threadID, ((error, info) => {
+                global.client.handleReaction.push({
+                    name: NpmCommand.config.name,
+                    messageID: info.messageID,
+                    author: event.senderID,
+                    type: "confirm",
+                    package: packages[packageIndex - 1]
+                });
+            }), event.messageID);
+        } else {
+            api.sendMessage("Invalid number", event.threadID, event.messageID);
+        }
+    }
+
+    static async handleReaction({ api, event, handleReaction }) {
+        const { userID: eventUserId, reaction: eventReaction, threadID: eventThreadID, } = event;
+
+        const {
+            author: handleReactionAuthor,
+            type: handleReactionType,
+            messageID: handleReactionMessageID,
+            package: { package: { name: packageName } },
+        } = handleReaction;
+
+        if (eventUserId !== handleReactionAuthor) {
+            return;
+        }
+
+        const SUCCESS_MESSAGE = `Package ${packageName} installed successfully 🌸`;
+        const ERROR_MESSAGE = `An error occurred while installing package ${packageName} ❗`;
+
+        if (handleReactionType === "confirm" && eventReaction === "❤") {
+            try {
+                api.unsendMessage(handleReactionMessageID);
+                api.sendMessage(`Please wait, the bot is installing package ${packageName}...`, eventThreadID);
+                const { stdout } = await NpmCommand.runCommand(`npm i ${packageName}`);
+                console.log(stdout);
+                api.sendMessage(SUCCESS_MESSAGE, eventThreadID);
+            } catch (error) {
+                console.error(`Error: ${error.message}`);
+                api.sendMessage(ERROR_MESSAGE, eventThreadID);
+            }
+        } else if (handleReactionType === "confirm" && eventReaction === "👎") {
+            api.unsendMessage(handleReactionMessageID);
+            api.sendMessage("Package installation canceled", eventThreadID);
+        }
+    }
+
+    static getPackage(packageName) {
+        return new Promise((resolve, reject) => {
+            https.get(`https://registry.npmjs.org/-/v1/search?text=${packageName}`, (res) => {
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    const response = JSON.parse(data);
+                    resolve({ data: response });
+                });
+            }).on('error', reject);
+        });
+    }
+
+    static runCommand(command) {
+        const { promisify } = require('util');
+        const exec = promisify(require('child_process').exec);
+        return exec(command);
+    }
+}
+
+module.exports = NpmCommand;
