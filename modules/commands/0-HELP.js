627cc67d5518df6341b52c8ffbea1912e01fb9a5
Shankar Suman <shankarproject29@gmail.com>
2024-10-17 21:31:44 +0530

    Update and rename help.js to 0-HELP.js

--- modules/commands/0-HELP.js
+++ modules/commands/0-HELP.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "help",
   version: "1.0.2",
   hasPermssion: 0,
-  credits: "SHANKAR-SUMAN",// don't change the credits kundi sisirain ko yan
+  credits: "SHANKAR-SUMAN",// don't change the credits SHANKAR-SUMAN
   description: "commands list",
   commandCategory: "system",
   usages: "module name",
