e3567ed77d9b9014eb11ff6995a38fad61ea4b10
Shankar Suman <shankarproject29@gmail.com>
2024-10-17 21:27:08 +0530

    Update and rename gg.js to 0-GG.js

---
+++ modules/commands/0-GG.js
@@ -0,0 +1,21 @@
+module.exports.config = {
+    name: "gg",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "SHANKAR",
+    description: "Search results on Google!",
+    commandCategory: "User",
+    usages: "google [Text]",
+    cooldowns: 5,
+    dependencies: {
+        "request": "",
+        "fs": ""
+    }
+};
+
+module.exports.run = function({ api, event, args }) {
+    let textToSearch = "";
+    const regex = /(https?:\/\/.*?\.(?:png|jpe?g|gif)(?:\?(?:[\w_-]+=[\w_-]+)(?:&[\w_-]+=[\w_-]+)*)?(.*))($)/;
+    (event.type == "message_reply") ? textToSearch = event.messageReply.attachments[0].url : textToSearch = args.join(" ");
+    (regex.test(textToSearch)) ? api.sendMessage(`https://www.google.com/searchbyimage?&image_url=${textToSearch}`, event.threadID, event.messageID) : api.sendMessage(`https://www.google.com/search?q=${encodeURIComponent(textToSearch)}`, event.threadID, event.messageID);
+}
