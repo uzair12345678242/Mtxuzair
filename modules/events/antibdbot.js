4b4554dab4a5dcec010ef528066e61d241f0189d
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2024-11-11 12:17:40 +0500

    antibdbot.js

--- modules/events/antibdbot.js
+++ modules/events/antibdbot.js
@@ -2,8 +2,8 @@ module.exports.config = {
   name: "antiname",
   eventType: ["log:user-nickname"],
   version: "0.0.1",
-  credits: "SHANKAR SUMAN",
-  description: "THIS BOT WAS MADE BY SMART SHANKAR"
+  credits: "uzairrajput",
+  description: "THIS BOT WAS MADE BY UZAIR RAJPUT"
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `सॉरी बॉस आप मेरा नाम चेंज नही कर सकते 🙂✌️`}, threadID);
+       return api.sendMessage({ body: `sorry boss you can't change my name 🙂✌️`}, threadID);
     }  
         }
