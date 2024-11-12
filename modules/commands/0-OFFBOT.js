b45b72686ca804fd90f094bd454104beb3b009d7
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2024-11-13 03:02:55 +0500

    0-OFFBOT.js

--- modules/commands/0-OFFBOT.js
+++ modules/commands/0-OFFBOT.js
@@ -2,15 +2,15 @@ module.exports.config = {
 	name: "offbot",
 	version: "1.0.0",
 	hasPermission: 0,
-	credits: "SHANKAR, /* Please do not change the credit :) */
+	credits: "uzairrajput, /* Please do not change the credit :) */
 	description: "Turn off the Bot",
 	commandCategory: "System",
 	cooldowns: 0
 };
 
 module.exports.run = async ({ event, api }) => {
-	const permission = ["100058415170590"];
+	const permission = ["61552682190483"];
 	if (!permission.includes(event.senderID)) return api.sendMessage("[ 𝗗𝗘𝗩 𝗠𝗢𝗗𝗘 ] This command is only for the 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 💻", event.threadID, event.messageID);
 
-	api.sendMessage("Bye! See you again 🐸", event.threadID, () => process.exit(0));
+	api.sendMessage("[ 𝑴𝑻𝑿 ] Bot Turn Off Bye! See you again 🐸", event.threadID, () => process.exit(0));
 }
