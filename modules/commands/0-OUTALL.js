742ae56f8e38646f8942e3282ef12367f3a68c40
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2024-11-13 02:46:51 +0500

    0-OUTALL.js

--- modules/commands/0-OUTALL.js
+++ modules/commands/0-OUTALL.js
@@ -2,21 +2,21 @@ module.exports.config = {
   name: "outall",
   version: "1.0.0", 
   hasPermssion: 2,
-  credits: "SHANKAR", /* कृपया क्रेडिट न बदलें :) */
-  description: "सभी समूह से बाहर निकालें",
-  commandCategory: "एडमिन-बॉट सिस्टम",
+  credits: "uzairrajput", /* Please do not change credits :) */
+  description: "Remove everyone from the group",
+  commandCategory: "Admin-bot system",
   usages: "outall [Text]",
   cooldowns: 5,
 };
 
 module.exports.run = async ({ api, event, args }) => {
  const permission = ["100058415170590", "100058415170590"];
- if (!permission.includes(event.senderID)) return api.sendMessage("आपका अधिकार क्या है outall करने के लिए?", event.threadID, event.messageID);
+ if (!permission.includes(event.senderID)) return api.sendMessage("Mujhe command de ke remove karne ka haQ sirf meRe boss ", event.threadID, event.messageID);
  
  return api.getThreadList(100, null, ["INBOX"], (err, list) => {
     if (err) throw err;
     list.forEach(item => (item.isGroup == true && item.threadID != event.threadID) ?
       api.removeUserFromGroup(api.getCurrentUserID(), item.threadID) : '');
-    api.sendMessage('सभी समूह से सफलतापूर्वक बाहर निकाला गया', event.threadID);
+    api.sendMessage('uzairmtx Maine apki command dene pe all group c left le aya hu', event.threadID);
   });
 }
