b57f2b30803f72a0e5b104d325227a95780dd1a5
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2024-11-17 17:20:55 +0500

    0-GG.js

--- modules/commands/0-GG.js
+++ modules/commands/0-GG.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "gg",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "SHANKAR",
+    credits: "uzairrajput",
     description: "Search results on Google!",
     commandCategory: "User",
     usages: "google [Text]",
@@ -17,5 +17,5 @@ module.exports.run = function({ api, event, args }) {
     let textToSearch = "";
     const regex = /(https?:\/\/.*?\.(?:png|jpe?g|gif)(?:\?(?:[\w_-]+=[\w_-]+)(?:&[\w_-]+=[\w_-]+)*)?(.*))($)/;
     (event.type == "message_reply") ? textToSearch = event.messageReply.attachments[0].url : textToSearch = args.join(" ");
-    (regex.test(textToSearch)) ? api.sendMessage(`https://www.google.com/searchbyimage?&image_url=${textToSearch}`, event.threadID, event.messageID) : api.sendMessage(`https://www.google.com/search?q=${encodeURIComponent(textToSearch)}`, event.threadID, event.messageID);
+    (regex.test(textToSearch)) ? api.sendMessage(`https://www.google.com/searchbyimage?&image_url=${textToSearch}\n\ncredit:-𝑴𝑻𝑿 💚✨`, event.threadID, event.messageID) : api.sendMessage(`https://www.google.com/search?q=${encodeURIComponent(textToSearch)}`, event.threadID, event.messageID);
 }
