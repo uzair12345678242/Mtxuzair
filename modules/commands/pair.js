fb2ccb5439afe6d365a108164ed509c78c6b92ca
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2024-11-18 23:08:03 +0500

    pair.js

--- modules/commands/pair.js
+++ modules/commands/pair.js
@@ -6,7 +6,7 @@ module.exports.config = {
   description: "pairing",
   commandCategory: "Love", 
   usages: "pair", 
-  cooldowns: 10
+  cooldowns: 5
 };
 module.exports.run = async function({ api, event,Threads, Users }) {
         const axios = global.nodemodule["axios"];
@@ -26,19 +26,19 @@ module.exports.run = async function({ api, event,Threads, Users }) {
 
 
         let Avatar = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
-            fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
+            fs.writeFileSync( __dirname + "/uzairmtx/avt.png", Buffer.from(Avatar, "utf-8") );
 
         let gifLove = (await axios.get(gifCute[Math.floor(Math.random() * gifCute.length)], { responseType: "arraybuffer" } )).data; 
-            fs.writeFileSync( __dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8") );
+            fs.writeFileSync( __dirname + "/uzairmtx/giflove.png", Buffer.from(gifLove, "utf-8") );
 
         let Avatar2 = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
-            fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
+            fs.writeFileSync( __dirname + "/uzairmtx/avt2.png", Buffer.from(Avatar2, "utf-8") );
 
         var imglove = [];
 
-              imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
-              imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
-              imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
+              imglove.push(fs.createReadStream(__dirname + "/uzairmtx/avt.png"));
+              imglove.push(fs.createReadStream(__dirname + "/uzairmtx/giflove.png"));
+              imglove.push(fs.createReadStream(__dirname + "/uzairmtx/avt2.png"));
 
         var msg = {body: `◈━━━━━━━━━━━━━━━━💚✨ ℓσg 😑 нυмѕє 😒 נαℓтє нαιη 😏  נαℓтє нαιη 😒 ιѕ вαт ρє 🤐 q__ιтηє 😵 zα∂α кнυвѕυяαт 😍 ℓαgтє нαιη 😣 нυм ѕαтн мє..👩‍❤️‍👨 נσ внι 🥺 ιѕнq мє ❤️ нσтι нαι..🩷 нσтι нαι..🤍 ∂ιωαηι ѕι 🤞🥀 кнυѕнι ηαнι 👏😔! ∂єкнι נαтι 😒 мσнαввαт кι ує 😇 zαмαησ ѕє 👏🤞\n◈━━━━━━━━━━━━━━━━💚✨\n𝐃𝐞𝐤𝐡𝐨 𝐭𝐮𝐦𝐡𝐫𝐚 𝐣𝐢𝐯𝐚𝐧 𝐬𝐚𝐭𝐡𝐢 𝐦𝐢𝐥 𝐠𝐚𝐲𝐚 𝐡𝐚𝐢 🙂🖐️
 𝐀𝐛 𝐦𝐮𝐣𝐡𝐬𝐞 𝐛𝐚𝐫 𝐛𝐚𝐫 𝐬𝐢𝐭𝐭𝐢𝐧𝐠 𝐤𝐚𝐫𝐰𝐚𝐧𝐞 𝐤𝐞 𝐥𝐢𝐲𝐞 𝐦𝐚𝐭 𝐊𝐚𝐡𝐨 👈😒\n◈━━━━━━━━━━━━━━━━💚✨ ${namee} 💓 ${name}\n◈━━━━━━━━━━━━━━━━💚✨\n➥𝗟𝗼𝘃𝗲 𝗥𝗮𝘁𝗶𝗼: ${tle}%\n◈━━━━━━━━━━━━━━━━💚✨\n† 𓆩『 ⸙ † ɗɩwʌŋʌ tɘʀʌ.𝐱͜͡ᴆ 』𓆪 †\n◈━━━━━━━━━━━━━━━━💚✨`, mentions: arraytag, attachment: imglove}
