83b4de29c642c9e936d3d5193f58d0c4a4d696c0
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2024-11-19 16:17:42 +0500

    0_MOTKI.js

--- modules/commands/0_MOTKI.js
+++ modules/commands/0_MOTKI.js
@@ -1,4 +1,4 @@
-qmodule.exports.config = {
+module.exports.config = {
   name: "motki",
   version: "7.3.1",
   hasPermssion: 0,
@@ -11,7 +11,7 @@ qmodule.exports.config = {
 module.exports.handleEvent = async function({ api, event, client, Users, __GLOBAL }) {
   var { threadID, messageID } = event;
   var name = await Users.getNameUser(event.senderID);
-  if (event.body.indexOf("moti")>=0 || event.body.indexOf("Mota")>=0 || event.body.indexOf("MOTI")>=0 || event.body.indexOf("MOTA")>=0 || event.body.indexOf("moti")>=0 || event.body.indexOf("Moti")>=0 || event.body.indexOf("mota")>=0 || event.body.indexOf("Moto")>=0 || event.body.indexOf("MOTO")>=0 || event.body.indexOf("SANDA")>=0 ) { 
+  if (event.body.indexOf("motki")>=0 || event.body.indexOf("Motka")>=0 || event.body.indexOf("Moti")>=0 || event.body.indexOf("Mota")>=0 || event.body.indexOf("moti")>=0 || event.body.indexOf("Moti")>=0 || event.body.indexOf("mota")>=0 || event.body.indexOf("Moto")>=0 || event.body.indexOf("MOTO")>=0 || event.body.indexOf("SANDA")>=0 ) { 
     var msg = {
         body: `  ${name} Mota pet sarakh pe let gari ai phat gaya pet gari ka number 101 mota bole haye meRa pet 😂🤣👈 `
       }
