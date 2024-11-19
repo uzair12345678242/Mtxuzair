8514e6a83203292034ae3b8d75e228d40bfbc83a
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2024-11-19 16:39:02 +0500

    Create 0_MAINAAGAYA.js

--- modules/commands/0_MAINAAGAYA.js
+++ modules/commands/0_MAINAAGAYA.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   if (event.body.indexOf("me agai")>=0 || event.body.indexOf("me agya")>=0 || event.body.indexOf("I'm agya")>=0 || event.body.indexOf("𝑴𝑻𝑿 💚✨")>=0) {
     var msg = {
         body: "welcome boss🙈",
-        attachment: fs.createReadStream(__dirname + `/uzair/dk.mp3`)
+        attachment: fs.createReadStream(__dirname + `/uzairmtx/dk.mp3`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😘", event.messageID, (err) => {}, true)
