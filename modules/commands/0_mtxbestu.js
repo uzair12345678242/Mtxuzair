92e87ff638e03c48e62d05010edf863777f44faf
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2024-11-24 14:50:33 +0500

    Create 0_mtxbestu.js

--- modules/commands/0_mtxbestu.js
+++ modules/commands/0_mtxbestu.js
@@ -19,8 +19,8 @@ module.exports.onLoad = async() => {
   const { resolve } = global.nodemodule["path"];
   const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
   const { downloadFile } = global.utils;
-  const dirMaterial = __dirname + `/uzair/mtx/`;
-  const path = resolve(__dirname, 'uzair/mtx', 'mtxbestu.png');
+  const dirMaterial = __dirname + `uzairmtx`;
+  const path = resolve(__dirname, 'uzairmtx', 'mtxbestu.png');
   if (!existsSync(dirMaterial + "mtx")) mkdirSync(dirMaterial, { recursive: true });
   if (!existsSync(path)) await downloadFile("https://i.imgur.com/RloX16v.jpg", path); 
 }
