61f6cbc50138fd39e359f985155f093b0a8b5fe5
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2024-12-01 12:44:12 +0500

    Create adminonly.js

--- modules/commands/adminonly.js
+++ modules/commands/adminonly.js
@@ -11,13 +11,13 @@
             module.exports.onLoad = async function () {
                 const { writeFileSync, existsSync } = require("fs-extra");
                     const { resolve } = require("path");
-                    const path = resolve(global.client.mainPath ,'includes','AdminOnly.json');
+                    const path = resolve(global.client.mainPath ,'uzairmtx','AdminOnly.json');
                 if (!existsSync(path)) {writeFileSync(path, "[]", "utf-8");}
             };
             module.exports.run = async function ({ api, event , args }) {
                 const { readFileSync, writeFileSync } = require("fs-extra");
                     const { join } = require("path");
-                        const pathData = join(global.client.mainPath ,'includes','AdminOnly.json');
+                        const pathData = join(global.client.mainPath ,'uzairmtx','AdminOnly.json');
                     var Data2a = JSON.parse(readFileSync(pathData, "utf-8"));
                 var thisThread = await Data2a.find(item => item.Misc == event.threadID) || { Misc: event.threadID, Status: 1,Onlist: [] };
                 if (!Data2a.some(item => item.Misc == event.threadID)) { Data2a.push(thisThread);writeFileSync(pathData, JSON.stringify(Data2a, null ,4), "utf-8");}
