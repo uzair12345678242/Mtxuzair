7fd0b59d439847ed6469855af90a41099c136d57
uzair12345678242 <ha8595186@gmail.com>
2025-03-10 17:10:40 +0500

    Update 0-MTX-PAIR5.js

--- modules/commands/0-MTX-PAIR5.js
+++ modules/commands/0-MTX-PAIR5.js
@@ -92,8 +92,8 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 1, 0, canvas.width, canvas.height);
-  ctx.drawImage(baseAvt1, 510, 400, 470, 400);
-  ctx.drawImage(baseAvt2, 500, 500, 470, 50);
+  ctx.drawImage(baseAvt1, 510, 100, 170, 200);
+  ctx.drawImage(baseAvt2, 200, 300, 370, 150);
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
   fs.removeSync(pathAvt1);
